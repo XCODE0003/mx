aeead0095eac6721170fa9821b5de4e660e9d3c9
jacobkiller1213 <jacobkiller1235@proton.me>
2025-11-01 00:41:12 +0500

    last

--- resources/views/pdf/task.blade.php
+++ resources/views/pdf/task.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Задание </title>
     <base href="{{ url('/') }}/">
-    <link rel="stylesheet" href="/assets/task.css">
+    <link rel="stylesheet" href="/assets/task.scss">
     <style>
         body {
             font-family: 'Times New Roman', serif;
