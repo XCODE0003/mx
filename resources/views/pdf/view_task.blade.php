991505b9a1e06f48ed00cfa093bdff9174dccda0
jacobkiller1213 <jacobkiller1235@proton.me>
2025-11-01 00:37:47 +0500

    last

--- resources/views/pdf/view_task.blade.php
+++ resources/views/pdf/view_task.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Задание </title>
     <base href="{{ url('/') }}/">
+    <link rel="stylesheet" href="/assets/task.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap">
     <style>
         body {
