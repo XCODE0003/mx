92d2057043c4fee83889324dfc3d920ea7cc775c
WORK <jacobkiller1235@proton.me>
2026-01-21 20:16:35 +0500

    last

--- database/migrations/2026_01_21_151210_add_field_blank.php
+++ database/migrations/2026_01_21_151210_add_field_blank.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('tasks', function (Blueprint $table) {
+        Schema::table('blank_texts', function (Blueprint $table) {
             $table->boolean('border')->default(false);
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('tasks', function (Blueprint $table) {
+        Schema::table('blank_texts', function (Blueprint $table) {
             $table->dropColumn('border');
         });
     }
