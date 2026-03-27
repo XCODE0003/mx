a418415cea235da315e05aa29a59ce860b7aacf5
WORK <jacobkiller1235@proton.me>
2026-03-27 14:24:08 +0500

    last

--- routes/web.php
+++ routes/web.php
@@ -68,9 +68,7 @@ Route::post('/variants/{uuid}/regenerate', [TaskExportController::class, 'regene
     ->whereUuid('uuid');
 
 
-Route::get('/mailru-domainpyRkzdZCNB0Cq5fn.html', function () {
-    return view('mailru-domainpyRkzdZCNB0Cq5fn');
-})->name('mailru-domainpyRkzdZCNB0Cq5fn');
+
 require __DIR__.'/auth.php';
 require __DIR__.'/profile.php';
 require __DIR__.'/settings.php';
